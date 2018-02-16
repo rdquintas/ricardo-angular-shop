2ebd9481b7689c6232a8b175c73a799c5c64bf17
rdquintas <rqcenter-web@yahoo.com>
2018-02-16 17:39:20 +0000

    adding Firebase stuff

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCqdD_6K2t8okrCUSi0Xz0DJnr4jiWdacg',
+    authDomain: 'ricardo-angular-shop.firebaseapp.com',
+    databaseURL: 'https://ricardo-angular-shop.firebaseio.com',
+    projectId: 'ricardo-angular-shop',
+    storageBucket: 'ricardo-angular-shop.appspot.com',
+    messagingSenderId: '816102345356'
+  }
 };
