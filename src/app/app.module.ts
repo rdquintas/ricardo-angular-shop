4a0c76d6b3d24b0b4f43c5cd3e09aa3910af78fb
rdquintas <rqcenter-web@yahoo.com>
2018-02-16 16:02:30 +0000

    adding home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ShoppingCartComponent
+    ShoppingCartComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
