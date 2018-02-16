d2fc25b024beced4c3a3e48aa878d1ed6b7485c8
rdquintas <rqcenter-web@yahoo.com>
2018-02-16 17:31:30 +0000

    adding Firebase stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { environment } from './../environments/environment';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 import { AppComponent } from './app.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
@@ -16,6 +19,8 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
     NgbModule.forRoot(),
     RouterModule.forRoot([{ path: '', component: HomeComponent }]),
     RouterModule.forRoot([{ path: 'shopping-cart', component: ShoppingCartComponent }])
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCqdD_6K2t8okrCUSi0Xz0DJnr4jiWdacg',
+    authDomain: 'ricardo-angular-shop.firebaseapp.com',
+    databaseURL: 'https://ricardo-angular-shop.firebaseio.com',
+    projectId: 'ricardo-angular-shop',
+    storageBucket: 'ricardo-angular-shop.appspot.com',
+    messagingSenderId: '816102345356'
+  }
 };
