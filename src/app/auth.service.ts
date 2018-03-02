1245339b0b7e5acf86c09bfaef691ef61a5ae577
rdquintas <rqcenter-web@yahoo.com>
2018-03-02 11:42:08 +0000

    adding ROUTE redirect

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -2,13 +2,14 @@ import { AngularFireAuth } from 'angularfire2/auth';
 import { Observable } from 'rxjs/Observable';
 import { Injectable } from '@angular/core';
 import * as firebase from 'firebase';
+import { ActivatedRoute } from '@angular/router';
 
 @Injectable()
 export class AuthService {
 
   user$: Observable<firebase.User>;
 
-  constructor(private afAuth: AngularFireAuth) {
+  constructor(private afAuth: AngularFireAuth, private route: ActivatedRoute) {
     this.user$ = afAuth.authState;
     // afAuth.authState.subscribe(user => {
     //   this.user = user;
@@ -17,6 +18,8 @@ export class AuthService {
   }
 
   login() {
+    const returnUrl = this.route.snapshot.queryParamMap.get('returnUrl') || '/';
+    localStorage.setItem('returnUrl', returnUrl);
     this.afAuth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
   }
 
